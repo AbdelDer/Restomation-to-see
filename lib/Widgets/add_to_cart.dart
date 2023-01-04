d6696cd624c51eb44baf9f2ade395365abcbd6b2
naqeeb <naqeeb9a@gmail.com>
2023-01-04 16:15:15 +0500

    add to cart design changes

--- lib/Widgets/add_to_cart.dart
+++ lib/Widgets/add_to_cart.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restomation/Provider/cart_provider.dart';
+import 'package:restomation/Utils/contants.dart';
+import 'package:restomation/Widgets/custom_loader.dart';
+import 'package:restomation/Widgets/custom_text.dart';
+
+import '../MVVM/Repo/Storage Service/storage_service.dart';
+import '../Utils/app_routes.dart';
+import 'custom_button.dart';
 
 class AddToCart extends StatefulWidget {
   final Map foodData;
-  final bool isCart;
+  final String restaurantsKey;
   const AddToCart({
     super.key,
     required this.foodData,
-    required this.isCart,
+    required this.restaurantsKey,
   });
 
   @override
@@ -19,6 +27,7 @@ class _AddToCartState extends State<AddToCart> {
   int initialValue = 0;
   @override
   Widget build(BuildContext context) {
+    final ref = StorageService.storage.ref().child(widget.foodData["image"]);
     Cart cart = context.watch<Cart>();
     int index = cart.cartItems.indexWhere((element) =>
         element["name"].toString().toLowerCase() ==
@@ -28,17 +37,164 @@ class _AddToCartState extends State<AddToCart> {
     }
     return InkWell(
       onTap: () {
-        if (initialValue == 0) {
-          setState(() {
-            initialValue++;
-          });
-          widget.foodData["quantity"] = initialValue;
-          if (widget.isCart) {
-            widget.foodData["price"] =
-                getDiscountedPrice(widget.foodData["price"]);
-          }
-          context.read<Cart>().addCartItem(widget.foodData);
-        }
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            int quantity = 1;
+
+            return Container(
+              color: kGrey.shade200,
+              height: 700,
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    color: kWhite,
+                    child: Row(
+                      children: [
+                        FutureBuilder(
+                            future: ref.getDownloadURL(),
+                            builder:
+                                (BuildContext context, AsyncSnapshot snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.done) {
+                                return Container(
+                                  width: 70,
+                                  height: 70,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(15),
+                                      boxShadow: const [
+                                        BoxShadow(
+                                            offset: Offset(0, 0),
+                                            spreadRadius: 2,
+                                            blurRadius: 2,
+                                            color: Colors.black12)
+                                      ],
+                                      image: DecorationImage(
+                                          image: NetworkImage(snapshot.data!),
+                                          fit: BoxFit.cover)),
+                                );
+                              }
+                              return Container(
+                                width: 70,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        offset: Offset(0, 0),
+                                        spreadRadius: 2,
+                                        blurRadius: 2,
+                                        color: Colors.black12)
+                                  ],
+                                ),
+                              );
+                            }),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: Text(
+                            widget.foodData["name"],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        StatefulBuilder(builder: (context, setState) {
+                          return Container(
+                            padding: const EdgeInsets.all(10),
+                            margin: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                color: primaryColor.withOpacity(0.1),
+                                borderRadius: BorderRadius.circular(10),
+                                border:
+                                    Border.all(color: primaryColor, width: 1)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                InkWell(
+                                    onTap: () {
+                                      if (quantity > 1) {
+                                        setState(() {
+                                          quantity--;
+                                        });
+                                      }
+                                    },
+                                    child: const Icon(
+                                      Icons.remove,
+                                      color: primaryColor,
+                                      size: 15,
+                                    )),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  quantity.toString(),
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                InkWell(
+                                    onTap: () {
+                                      setState(() {
+                                        quantity++;
+                                      });
+                                    },
+                                    child: const Icon(
+                                      Icons.add,
+                                      color: primaryColor,
+                                      size: 15,
+                                    ))
+                              ],
+                            ),
+                          );
+                        }),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: StreamBuilder(
+                        stream: FirebaseDatabase.instance
+                            .ref()
+                            .child("menu_items")
+                            .child(widget.restaurantsKey)
+                            .orderByChild("upselling")
+                            .equalTo(true)
+                            .onValue,
+                        builder:
+                            (context, AsyncSnapshot<DatabaseEvent?> snapshot) {
+                          return Builder(builder: (context) {
+                            Cart cart = context.watch<Cart>();
+                            return menuItemsView(
+                                snapshot, cart, widget.foodData["name"]);
+                          });
+                        }),
+                  ),
+                  CustomButton(
+                      buttonColor: primaryColor,
+                      text: "Add Item",
+                      textColor: kWhite,
+                      function: () {
+                        Cart cart = context.read<Cart>();
+                        widget.foodData["quantity"] = quantity;
+                        cart.addCartItem(widget.foodData);
+                        KRoutes.pop(context);
+                      }),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            );
+          },
+        );
       },
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 5),
@@ -99,9 +255,140 @@ class _AddToCartState extends State<AddToCart> {
     );
   }
 
+  Widget menuItemsView(
+      AsyncSnapshot<DatabaseEvent?> snapshot, Cart cart, String name) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(child: CustomLoader());
+    }
+    if (snapshot.data!.snapshot.children.isEmpty) {
+      return const Center(
+          child: CustomText(text: "No upselling items added yet !!"));
+    }
+    Map allrestaurantsMenuItems = snapshot.data!.snapshot.value as Map;
+    List categoriesListItems = allrestaurantsMenuItems.keys.toList();
+
+    categoriesListItems =
+        allrestaurantsMenuItems.keys.toList().where((element) {
+      final categoryTitle =
+          allrestaurantsMenuItems[element]["status"].toString().toLowerCase();
+      const status = "available";
+      return categoryTitle == status;
+    }).toList();
+
+    categoriesListItems =
+        allrestaurantsMenuItems.keys.toList().where((element) {
+      final categoryTitle =
+          allrestaurantsMenuItems[element]["name"].toString().toLowerCase();
+
+      return categoryTitle != name.toLowerCase();
+    }).toList();
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      decoration:
+          BoxDecoration(color: kWhite, borderRadius: BorderRadius.circular(15)),
+      child: Column(
+        children: [
+          const Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CustomText(
+                  text: "Special offers only for you :",
+                  fontWeight: FontWeight.bold,
+                  fontsize: 15,
+                ),
+              )),
+          Expanded(
+            child: ListView.separated(
+              itemCount: categoriesListItems.length,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              itemBuilder: (context, index) {
+                String key = categoriesListItems[index];
+                Map foodItem = allrestaurantsMenuItems[key] as Map;
+                foodItem["key"] = key;
+                int index2 = cart.cartItems.indexWhere((element) =>
+                    element["name"].toString().toLowerCase() ==
+                    foodItem["name"].toString().toLowerCase());
+
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.adjust_rounded,
+                          color:
+                              foodItem["type"].toString().toLowerCase() == "veg"
+                                  ? Colors.green
+                                  : Colors.red,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              foodItem["name"],
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "Rs. ${foodItem["price"]}  ",
+                                  style: const TextStyle(
+                                    color: kGrey,
+                                    decoration: TextDecoration.lineThrough,
+                                  ),
+                                ),
+                                CustomText(
+                                  text:
+                                      "Rs. ${getDiscountedPrice(foodItem["price"])}  ",
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                const CustomText(
+                                  text: "( You save Rs. 20 )",
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Checkbox(
+                        value: index2 != -1 ? true : false,
+                        onChanged: (value) {
+                          if (index2 == -1) {
+                            foodItem["quantity"] = 1;
+                            foodItem["price"] =
+                                getDiscountedPrice(foodItem["price"]);
+                            cart.addCartItem(foodItem);
+                          } else {
+                            cart.removeCartItem(foodItem);
+                          }
+                        })
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => const Divider(
+                color: kGrey,
+                // indent: 100,
+                // endIndent: 100,
+                thickness: 1,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   String getDiscountedPrice(String price) {
     double totalPrice = double.parse(price);
-    totalPrice = totalPrice - (totalPrice * 0.1);
-    return "$totalPrice";
+    totalPrice = totalPrice - 20;
+    return totalPrice.toStringAsFixed(0);
   }
 }
